25d0d1349bf7601541a1826bf89403ced09b861b
Павел Зайцев <sirzaitsev@yandex.ru>
2025-08-31 18:08:55 +0300

    real

---
+++ src/Category.py
@@ -0,0 +1,22 @@
+from src.Product import Product
+
+class Category:
+    """
+    Класс для описания категории товаров.
+
+    """
+    name: str
+    description: str
+    products: list
+
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name: str, description: str, products: [Product]) -> None:
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
