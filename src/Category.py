6a4f6ef29bae2f3ae01ac015eb502eaa942dea83
Павел Зайцев <sirzaitsev@yandex.ru>
2025-09-08 12:26:39 +0300

    self

--- src/Category.py
+++ src/Category.py
@@ -20,4 +20,4 @@ class Category:
         self.products = products
 
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.products)
