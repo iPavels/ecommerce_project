04f065c380ac190608a8a8da565dda65176909e3
Павел Зайцев <sirzaitsev@yandex.ru>
2025-10-14 12:49:28 +0300

    modcatpro

--- tests/test_product.py
+++ tests/test_product.py
@@ -31,3 +31,26 @@ def test_product_new_product():
     assert product.name == "Ноутбук"
     assert product.price == 80000
     assert product.quantity == 2
+
+
+#  Новый тест для строкового представления продукта (__str__)
+def test_product_str():
+    p = Product("Телефон", "Смартфон", 80, 15)
+    assert str(p) == "Телефон, 80 руб. Остаток: 15 шт."
+
+
+# 🔹 Новый тест для корректной работа сложения двух продуктов (__add__)
+def test_product_add():
+    a = Product("Телефон", "Смартфон", 100, 10)
+    b = Product("Ноутбук", "Игровой", 200, 2)
+    result = a + b
+    assert result == 1400  # 100*10 + 200*2
+
+
+# 🔹 Новый тест проверки ошибки при сложении с неподдерживаемым типом
+def test_product_add_type_error():
+    a = Product("Телефон", "Смартфон", 100, 10)
+    with pytest.raises(
+        TypeError, match="Складывать можно только объекты класса Product"
+    ):
+        a + 10
