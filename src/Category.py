cd84a0e24c716833bce9aa4960b6d53bee28b795
Павел Зайцев <sirzaitsev@yandex.ru>
2025-12-04 11:38:33 +0300

    исключ

--- src/Category.py
+++ src/Category.py
@@ -33,14 +33,23 @@ class Category:
         Category.product_count += 1
 
     @property
-    def products(self) -> str:
-        """Геттер: возвращает строку со всеми продуктами"""
-        return "\n".join(str(product) for product in self.__products)
+    def products(self) -> list[Product]:
+        return self.__products
 
     @classmethod
     def new_product(cls, data: dict) -> Product:
         """Создает новый объект Product из словаря"""
         return Product.new_product(data)
+
+    def get_average_price(self):
+        """Возвращает среднюю цену товаров в категории"""
+        try:
+            total = sum(product.price for product in self.__products)
+            count = len(self.__products)
+            return total / count
+        except ZeroDivisionError:
+            return 0
+
 
     def __str__(self) -> str:
         """Строковое отображение категории"""
--- src/Product.py
+++ src/Product.py
@@ -9,6 +9,9 @@ class Product:
     quantity: int
 
     def __init__(self, name, description, price, quantity):
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
+
         self.name = name
         self.description = description
         self.__price = price
